fd50786adb8fd9beac228bb28bbd0661b82c3183
kamilmoskal <kamil_moskal@interia.pl>
2019-01-04 14:08:38 +0100

    mixed content error fix i hope so

--- src/getWeatherAction.js
+++ src/getWeatherAction.js
@@ -1,6 +1,6 @@
 export const getWeather = (city) => {
     return (dispatch) => {
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city.name},${city.code}&APPID=6f7f051af44ea396d568620c70082877`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city.name},${city.code}&APPID=6f7f051af44ea396d568620c70082877`)
         .then(res => res.json())
         .then(data => {
             dispatch({ type: 'GET_WEATHER', data});
