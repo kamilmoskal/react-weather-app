ed77981c78debbad95e1651befc36cfcc8cdc9ce
kamilmoskal <kamil_moskal@interia.pl>
2019-01-04 13:57:27 +0100

    Updates

---
+++ precache-manifest.114468a1a7cf13377bf731291a42d9e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72a6b7ef5541b624727a",
+    "url": "/react-weather-app/static/js/runtime~main.72a6b7ef.js"
+  },
+  {
+    "revision": "52478eec9555900aa0a3",
+    "url": "/react-weather-app/static/js/main.52478eec.chunk.js"
+  },
+  {
+    "revision": "7809a06ac85fc2de2033",
+    "url": "/react-weather-app/static/js/1.7809a06a.chunk.js"
+  },
+  {
+    "revision": "52478eec9555900aa0a3",
+    "url": "/react-weather-app/static/css/main.7203beb2.chunk.css"
+  },
+  {
+    "revision": "7809a06ac85fc2de2033",
+    "url": "/react-weather-app/static/css/1.de841a59.chunk.css"
+  },
+  {
+    "revision": "ded38ed198f057b7d885e9ef1bf5028e",
+    "url": "/react-weather-app/index.html"
+  }
+];
